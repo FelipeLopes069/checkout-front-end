468da6c62487ce1e42bcccc4ec06839d8c0f147b
FelipeLopes069 <felipelopestrindade888@gmail.com>
2025-05-28 12:48:57 -0300

    fix: corrige baseURL do axios para produção

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,5 +1,5 @@
 // Base da API (pode ser substituído por variável de ambiente se necessário)
-const baseURL = "encouraging-susanne-fynancce-v2-b9604e3e.koyeb.app/";
+const baseURL = " https://encouraging-susanne-fynancce-v2-b9604e3e.koyeb.app";
 
 /**
  * Recupera o token JWT armazenado localmente.
